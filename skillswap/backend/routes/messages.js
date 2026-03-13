11ee1b886e781606abd1e5937927392a7aecba41
Abhi-205 <abhayjadhav205@gmail.com>
2026-03-13 18:03:10 +0530

    add delete route

--- skillswap/backend/routes/messages.js
+++ skillswap/backend/routes/messages.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getMessages, sendMessage, generateMeetLink } = require('../controllers/messageController');
+const { getMessages, sendMessage, generateMeetLink, deleteMessage } = require('../controllers/messageController');
+router.delete('/:messageId', protect, deleteMessage);
 const { protect } = require('../middleware/auth');
 
 router.get('/:userId', protect, getMessages);
 router.post('/:userId', protect, sendMessage);
 router.post('/:userId/meet', protect, generateMeetLink);
+
 
 module.exports = router;
