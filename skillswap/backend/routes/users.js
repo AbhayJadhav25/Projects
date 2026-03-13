bfdfbaadab7265afd2f6846c0cafc21182f30201
Abhi-205 <abhayjadhav205@gmail.com>
2026-03-13 17:49:28 +0530

    Fix users.js

--- skillswap/backend/routes/users.js
+++ skillswap/backend/routes/users.js
@@ -8,8 +8,6 @@ router.get('/', protect, getAllUsers);
 router.get('/learning-hub', protect, getLearningHub);
 router.get('/:id', protect, getUserById);
 router.put('/profile', protect, uploadProfilePhoto.single('profilePhoto'), updateProfile);
-
-const { updateProfile, getAllUsers, getUserById, getLearningHub, rateUser } = require('../controllers/userController');
-
 router.post('/:id/rate', protect, rateUser);
+
 module.exports = router;
